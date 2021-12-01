0e8c5ed12f523a9fd9fffd9b1fed178d97deda4e
elenayran <36763284+elenayran@users.noreply.github.com>
2021-12-01 22:47:12 +0300

    oyuncu secenekleri playerprefs zorluk, ses ,seviye

--- Assets/Scripts/OyuncuAyarlari.cs
+++ Assets/Scripts/OyuncuAyarlari.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OyuncuAyarlari : MonoBehaviour
 {
@@ -25,7 +26,53 @@ public class OyuncuAyarlari : MonoBehaviour
     {
         return PlayerPrefs.GetFloat(ANA_SES_ANAHTARI);
     }
+    public static void SeviyeninKilidi(int seviye)
+    {
+        if (seviye < SceneManager.sceneCountInBuildSettings)
+        {
 
+                                //seviye_acik_0
+                                // seviye_acik_1 1 acik artik
+            PlayerPrefs.SetInt(SEVIYE_ANAHTARI + seviye.ToString(), 1);
+        }
+        else
+        {
+            Debug.LogError("Oyunda Olmayan bir sahne acilamaz.");
+        }
+    }
 
+// seviyenin kilidinin acik olup olmadigini kontrol eden fonk.
+    public static bool SeviyeAcikMi(int seviye)
+    {
+        int seviyeDegeri = PlayerPrefs.GetInt(SEVIYE_ANAHTARI + seviye.ToString());
+        bool SeviyeAcikMi = (seviyeDegeri == 1);
+        if (seviye < SceneManager.sceneCountInBuildSettings)
+        {
+            return SeviyeAcikMi;
+        }
+        else
+        {
+            Debug.LogError("Oyunda olmayan seviyenin kilidini sorgulayamazsin.");
+            return false;
+        }
+    }
 
+    // zorlugu ayarlayacak bir fonksiyon
+    public static void ZorluguAyarla(float zorluk)
+    {
+        if (zorluk >=0f && zorluk<=1)
+        {
+            PlayerPrefs.SetFloat(ZORLUK_ANAHTARI, zorluk);
+        }
+        else
+        {
+            Debug.LogError("zorluk seviyesi [0,1] degeri arasinda olmalidir");
+        }
+    }
+
+    // zorlugun degerini gösterecek bir fonksiyon yazilacak
+    public static float ZorluguAl()
+    {
+        return PlayerPrefs.GetFloat(ZORLUK_ANAHTARI);
+    }
 }
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Ses Baslangic Degeri:" + OyuncuAyarlari.AnaSesAl());
+        //OyuncuAyarlari.AnaSesiAyarla(0.5f);
+        //Debug.Log("Ses Ayarlandiktan Sonraki Deger:" + OyuncuAyarlari.AnaSesAl());
+
+        //Debug.Log(OyuncuAyarlari.SeviyeAcikMi(2));
+        //OyuncuAyarlari.SeviyeninKilidi(2);
+        //Debug.Log(OyuncuAyarlari.SeviyeAcikMi(2));
+
+        //Debug.Log(OyuncuAyarlari.ZorluguAl());
+        //OyuncuAyarlari.ZorluguAyarla(0.25f);
+        //Debug.Log(OyuncuAyarlari.ZorluguAl());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

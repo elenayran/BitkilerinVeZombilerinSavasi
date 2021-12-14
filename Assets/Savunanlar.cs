06f64d7372f1106e8bdf3519b375d9d19682da21
elenayran <36763284+elenayran@users.noreply.github.com>
2021-12-15 00:52:23 +0300

    objeler arasındaki etkileşim ayarlandı

--- Assets/Savunanlar.cs
+++ Assets/Savunanlar.cs
@@ -2,21 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Saldiranlar : MonoBehaviour
+public class Savunanlar : MonoBehaviour
 {
-    [Range(0f,2f)]
-    public float WalkSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        Rigidbody2D rigidbody = gameObject.AddComponent<Rigidbody2D>();
-        rigidbody.isKinematic = true;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * WalkSpeed * Time.deltaTime);
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {

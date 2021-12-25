9b3070f02c1f300a124d9abe3964abbccfb9fb47
elenayran <36763284+elenayran@users.noreply.github.com>
2021-12-25 23:46:55 +0300

    OBJELERE CAN EKLENDİ

--- Assets/Scripts/Saldiranlar.cs
+++ Assets/Scripts/Saldiranlar.cs
@@ -7,17 +7,23 @@ public class Saldiranlar : MonoBehaviour
     //[Range(0f,2f)]
     private float SuankiHiz;
     private GameObject mevcutHedef;
+    private Animator objeninAnimatoru;
     // Start is called before the first frame update
     void Start()
     {
         //Rigidbody2D rigidbody = gameObject.AddComponent<Rigidbody2D>();
         //rigidbody.isKinematic = true;
+        objeninAnimatoru = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.left * SuankiHiz * Time.deltaTime);
+        if (!mevcutHedef)
+        {
+            objeninAnimatoru.SetBool("SaldiriVarMi", false);
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -29,7 +35,14 @@ public class Saldiranlar : MonoBehaviour
     }
     public void ZararVer(float zararMiktari)
     {
-
+        if (mevcutHedef)
+        {
+            Saglik saglik = mevcutHedef.GetComponent<Saglik>();
+            if (saglik)
+            {
+                saglik.ZararAl(zararMiktari);
+            }
+        }
     }
     public void HedefiBelirle(GameObject hedefimiz)
     {
